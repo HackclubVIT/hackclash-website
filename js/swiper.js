d781bd11685092c6cfcca38391f8a0e1e760d4ec
Aakash <aakashsivakumar02@gmail.com>
2022-02-16 19:16:18 +0530

    merged with 20thfeb commit

---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  breakpoints: {
+    520: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 50
+    },
+    
+}
+
+});
