6f9221559b6ea0dab325010c2303b63d8c2faf57
Aakash <aakashsivakumar02@gmail.com>
2022-02-20 15:06:07 +0530

    fixed details and prizes section

--- js/swiper.js
+++ js/swiper.js
@@ -26,6 +26,25 @@ const swiper = new Swiper(".swiper", {
         spaceBetweenSlides: 50
     },
     
-}
+},
+calculateHeight:true,
+onSlideChangeStart: function(){
+  document.querySelector(".swiper li").removeClass('active')
+  document.querySelector(".swiper li").eq(tabsSwiper.activeIndex).classList.add('active')  
+},  
+  autoHeight: true,
+
 
 });
+
+document.querySelector(Window).onresize(function(){
+  var height = document.querySelector(window).height();
+  var width  = document.querySelector(window).width();
+  document.querySelector('.swiper, .swiper-slide').height(height);
+  document.querySelector('.swiper, .swiper-slide').width(width);
+  document.querySelector('.swiper-wrapper, .swiper-slide').height(height);
+  document.querySelector('.swiper-wrapper, .swiper-slide').width(width);
+  //Add reInit, because jQuery's resize event handler may occur earlier than Swiper's one
+  swiper.reInit()
+})
+document.querySelector(Window).onresize();
